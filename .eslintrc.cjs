b255b6f56283e51af9c1daa46a4922c4bcc5f570
Sega <Segabelka@yandex.ru>
2023-08-30 12:07:02 +0300

    feat: add commitlint for project and setup linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,86 +1,82 @@
-
 module.exports = {
   env: {
     browser: true,
     es2021: true,
   },
   extends: [
-    'airbnb',
-    'airbnb-typescript',
-    'airbnb/hooks',
-    'plugin:react/recommended',
-    'plugin:prettier/recommended',
-    'plugin:react/jsx-runtime',
+    "airbnb",
+    "airbnb-typescript",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "plugin:react/jsx-runtime",
   ],
   overrides: [
     {
-      files: ['config/**/*.ts', 'src/global-definitions.d.ts', 'src/libs.d.ts'],
+      files: ["config/**/*.ts", "src/global-definitions.d.ts", "src/libs.d.ts"],
       rules: {
-        'import/no-default-export': 'off',
+        "import/no-default-export": "off",
       },
     },
     {
-      files: ['src/redux/slices/*.ts'],
+      files: ["src/redux/slices/*.ts"],
       rules: {
-        'no-param-reassign': 'off',
-        'jsx-a11y/label-has-associated-control': 'off',
+        "no-param-reassign": "off",
+        "jsx-a11y/label-has-associated-control": "off",
       },
     },
     {
-      files: ['**/*.tsx'],
+      files: ["**/*.tsx"],
       rules: {
-        'react/require-default-props': 'off',
-        'consistent-return': 'off',
+        "react/require-default-props": "off",
+        "consistent-return": "off",
       },
     },
     {
       parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        project: './tsconfig.json',
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: "./tsconfig.json",
       },
-      extends: ['plugin:@typescript-eslint/recommended', "next/core-web-vitals"],
-      files: ['*.ts', '*.tsx'], 
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "next/core-web-vitals",
+      ],
+      files: ["*.ts", "*.tsx"],
     },
   ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react', '@typescript-eslint', 'prettier'],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react", "@typescript-eslint", "prettier"],
   rules: {
-    'jsx-a11y/label-has-associated-control': [
-      'error',
+    "jsx-a11y/label-has-associated-control": [
+      "error",
       {
         required: {
-          some: ['nesting', 'id'],
+          some: ["nesting", "id"],
         },
       },
     ],
-    'jsx-a11y/label-has-for': [
-      'error',
+    "jsx-a11y/label-has-for": [
+      "error",
       {
         required: {
-          some: ['nesting', 'id'],
+          some: ["nesting", "id"],
         },
       },
     ],
-    'import/no-extraneous-dependencies': [
-      'error',
+    "import/no-extraneous-dependencies": [
+      "error",
       {
-        devDependencies: ['**/*.test.{ts,tsx,js,jsx}'],
+        devDependencies: ["**/*.test.{ts,tsx,js,jsx}"],
       },
     ],
-    'import/no-default-export': 'error',
-    'import/prefer-default-export': 'off',
-    'react/react-in-jsx-scope': 0,
-    indent: 'off',
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^action' }],
-    'no-nested-ternary': 'off',
-    'no-unneeded-ternary': 'off',
-    'react/function-component-definition': [
-      'error',
-      {
-        namedComponents: 'arrow-function',
-        unnamedComponents: 'arrow-function',
-      },
+    "import/prefer-default-export": "off",
+    "react/react-in-jsx-scope": 0,
+    indent: "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { argsIgnorePattern: "^action" },
     ],
+    "no-nested-ternary": "off",
+    "no-unneeded-ternary": "off",
   },
 };
--- next-sitemap.config.cjs
+++ next-sitemap.config.cjs
@@ -3,9 +3,9 @@
  * @see https://github.com/iamvishnusankar/next-sitemap#readme
  */
 module.exports = {
-    siteUrl: '',
-    generateRobotsTxt: true,
-    robotsTxtOptions: {
-      policies: [{ userAgent: '*', allow: '/' }],
-    },
-  };
+  siteUrl: "",
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [{ userAgent: "*", allow: "/" }],
+  },
+};
