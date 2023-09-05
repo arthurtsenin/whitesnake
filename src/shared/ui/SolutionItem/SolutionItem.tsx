51b86f2dd42e16bcf777605c42000ae871523ac3
arthurtsenin <arturcenin@gmail.com>
2023-09-05 13:01:57 +0300

    feat: add solutionItem for cases

---
+++ src/shared/ui/SolutionItem/SolutionItem.tsx
@@ -0,0 +1,32 @@
+import classNames from "classnames";
+import Image from "next/image";
+import { FC } from "react";
+
+import styles from "./SolutionItem.module.scss";
+
+type SolutionItemProps = {
+  text: Array<string>;
+  image: string;
+  direction?: string;
+};
+
+export const SolutionItem: FC<SolutionItemProps> = ({
+  text,
+  image,
+  direction,
+}) => {
+  return (
+    <div
+      className={classNames([styles.container, styles[direction || ""]], {})}
+    >
+      <div className={styles.description}>
+        {text.map((paragraph) => (
+          <p key={paragraph}>{paragraph}</p>
+        ))}
+      </div>
+      <div className={styles.wrapper}>
+        <Image src={image} alt={image} />
+      </div>
+    </div>
+  );
+};
