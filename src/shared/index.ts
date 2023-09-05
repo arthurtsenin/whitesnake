bf1213dfe76b12ad805cc23bb6c34ae0b40357c3
arthurtsenin <arturcenin@gmail.com>
2023-09-05 13:03:57 +0300

    feat: add re-exports for shared folder

---
+++ src/shared/index.ts
@@ -0,0 +1,19 @@
+import { Button } from "./ui/Button/Button";
+import { Chip } from "./ui/Chip/Chip";
+import { Container } from "./ui/Container/Container";
+import { Input } from "./ui/Input/Input";
+import { SolutionContainer } from "./ui/SolutionContainer/SolutionContainer";
+import { SolutionItem } from "./ui/SolutionItem/SolutionItem";
+import { Textarea } from "./ui/Textarea/Textarea";
+import { Title } from "./ui/Title/Title";
+
+export {
+  Button,
+  Chip,
+  Container,
+  Input,
+  SolutionContainer,
+  SolutionItem,
+  Textarea,
+  Title,
+};
