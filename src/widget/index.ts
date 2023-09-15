88d8d33462a3134c56cca25cf52d58f793e8ff4f
arthurtsenin <arturcenin@gmail.com>
2023-09-15 12:49:40 +0300

    fix: widget re-exports

--- src/widget/index.ts
+++ src/widget/index.ts
@@ -1,6 +1,4 @@
 import { Accordion } from "./Accordion/Accordion";
-import { Loop } from "./Loop/Loop";
-import { Slider } from "./Slider/Slider";
 import { Tabs } from "./Tabs/Tabs";
 
-export { Accordion, Loop, Slider, Tabs };
+export { Accordion, Tabs };
