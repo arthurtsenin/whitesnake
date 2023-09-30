43507285939c59d41845bea896fc5346a40841a1
arthurtsenin <arturcenin@gmail.com>
2023-09-30 12:53:22 +0300

    feat: add styles for button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import { Button } from "@/shared";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "32px",
+        padding: "50px",
+      }}
+    >
+      <Button variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="transparent">Transparent</Button>
+    </main>
+  );
 }
