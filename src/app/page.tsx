ec3e7de74619e4e38fcb9b225a6cd280e138840f
arthurtsenin <arturcenin@gmail.com>
2023-09-30 13:59:01 +0300

    fix: remove useless import from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,3 @@
-import { Button } from "@/shared";
-
 export default function Home() {
-  return (
-    <main
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        gap: "32px",
-        padding: "50px",
-      }}
-    >
-      <Button variant="primary">Primary</Button>
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="transparent">Transparent</Button>
-    </main>
-  );
+  return <main></main>;
 }
