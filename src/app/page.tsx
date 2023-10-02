fc43d21dd286e8f92fb993dbeb4e1d3f9ddef1f8
Sega <Segabelka@yandex.ru>
2023-10-02 14:35:15 +0300

    fix: remove component from the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,3 @@
-import { ContactCard } from "../shared";
-
 export default function Home() {
-  return (
-    <main>
-      dsffd
-      <ContactCard
-        url="lala"
-        description="laladfl"
-        title="lalala"
-        key="lalalal"
-      />
-    </main>
-  );
+  return <main></main>;
 }
