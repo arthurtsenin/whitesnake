996405cd6156e657154019ecacf7561a6535b349
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-10-03 15:19:10 +0300

    feat: implement widget for contacts

---
+++ src/widget/Contacts/Contacts.tsx
@@ -0,0 +1,34 @@
+import { FC } from "react";
+
+import styles from "./Contacts.module.css";
+
+import { Title } from '@/shared/ui/Title/Title';
+import { TitleProps } from "@/shared/ui/Title/Title";
+import { ContactCard } from "@/shared/ui/ContactCard/ContactCard";
+
+import { ContactType } from "./data";
+
+export type ContactsProps = TitleProps & {
+  contacts: Array<ContactType>;
+};
+
+export const Contacts: FC<ContactsProps> = ({ title, subtitle, contacts }) => {
+  return (
+    <section className={styles.container}>
+      <Title
+        title={title}
+        subtitle={subtitle}
+      />
+      <div className={styles.contactsWrapper}>
+        {contacts.map((contact) => (
+          <ContactCard
+            key={contact.id}
+            url={contact.url}
+            title={contact.title}
+            description={contact.description}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
