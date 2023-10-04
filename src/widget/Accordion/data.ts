4cf71ab1e51df8a1dd8a85093ed9213b589ff090
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-10-04 12:38:47 +0300

    feat: fill accordion data in

--- src/widget/Accordion/data.ts
+++ src/widget/Accordion/data.ts
@@ -7,32 +7,38 @@ export type AccordionType = {
 export const ACCORDIONS_DATA: Array<AccordionType> = [
   {
     id: 1,
-    summary: "",
-    description: "",
+    summary: "Где вы находитесь?",
+    description:
+      "Все разработчики находятся в Минске. Удаленно никто не работает. Таким образом, мы можем поддерживать высокую производительность и можем предложить вам гарантии. Мы позаботимся о налогах, социальных выплатах, отпусках, больничных листах, инфраструктуре (ноутбуки, питание, спорт, занятия английским языком с личным учителем, страхование) - чтобы вам не пришлось.",
   },
   {
     id: 2,
-    summary: "",
-    description: "",
+    summary: "Есть ли у вас специалисты с опытом в нашей нише?",
+    description:
+      "Nam quis nulla. Integer malesuada. In in enim a arcu imperdiet malesuada. Sed vel lectus. Donec odio urna, tempus molestie, porttitor ut, iaculis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper pulvinar. Vestibulum fermentum tortor id mi. Pellentesque ipsum. Nulla non arcu lacinia neque faucibus fringilla. Nulla non lectus sed nisl molesti",
   },
   {
     id: 3,
-    summary: "",
-    description: "",
+    summary: "У вас есть опыт с Blockchain?",
+    description:
+      "Nam quis nulla. Integer malesuada. In in enim a arcu imperdiet malesuada. Sed vel lectus. Donec odio urna, tempus molestie, porttitor ut, iaculis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper pulvinar. Vestibulum fermentum tortor id mi. Pellentesque ipsum. Nulla non arcu lacinia neque faucibus fringilla. Nulla non lectus sed nisl molesti",
   },
   {
     id: 4,
-    summary: "",
-    description: "",
+    summary: "Какие есть гарантии?",
+    description:
+      "Nam quis nulla. Integer malesuada. In in enim a arcu imperdiet malesuada. Sed vel lectus. Donec odio urna, tempus molestie, porttitor ut, iaculis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper pulvinar. Vestibulum fermentum tortor id mi. Pellentesque ipsum. Nulla non arcu lacinia neque faucibus fringilla. Nulla non lectus sed nisl molesti",
   },
   {
     id: 5,
-    summary: "",
-    description: "",
+    summary: "Как быстро мы можем начать работу?",
+    description:
+      "Nam quis nulla. Integer malesuada. In in enim a arcu imperdiet malesuada. Sed vel lectus. Donec odio urna, tempus molestie, porttitor ut, iaculis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper pulvinar. Vestibulum fermentum tortor id mi. Pellentesque ipsum. Nulla non arcu lacinia neque faucibus fringilla. Nulla non lectus sed nisl molesti",
   },
   {
     id: 6,
-    summary: "",
-    description: "",
+    summary: "Как мы поймем, что ваша компания — это то что нам нужно?",
+    description:
+      "Nam quis nulla. Integer malesuada. In in enim a arcu imperdiet malesuada. Sed vel lectus. Donec odio urna, tempus molestie, porttitor ut, iaculis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper pulvinar. Vestibulum fermentum tortor id mi. Pellentesque ipsum. Nulla non arcu lacinia neque faucibus fringilla. Nulla non lectus sed nisl molesti",
   },
 ];
