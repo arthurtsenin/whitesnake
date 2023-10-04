a6cca0d9b72f31f2ad2363844bb6f81a4fd93f5b
arthurtsenin <arturcenin@gmail.com>
2023-10-04 19:01:18 +0300

    fix: remove useless components from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,3 @@
-import { Contacts } from "@/widget/Contacts/Contacts";
-import { CONTACTS_DATA } from "@/widget/Contacts/data";
-import { CASES_PREVIEW_CARDS } from "@/widget/GridContainer/data";
-import { GridContainer } from "@/widget/GridContainer/GridContainer";
-
 export default function Home() {
-  return (
-    <main
-      className="main"
-      style={{
-        overflow: "hidden",
-        display: "flex",
-        flexDirection: "column",
-        gap: "80px",
-      }}
-    >
-      <GridContainer
-        data={CASES_PREVIEW_CARDS}
-        subtitle="dfdfdf"
-        title="dfdfdf"
-        suptitle="dfdfdf"
-      />
-      <Contacts
-        title="Контакты"
-        subtitle="Набери нам"
-        contacts={CONTACTS_DATA}
-      />
-    </main>
-  );
+  return <main className="main"></main>;
 }
