5b928e5bf5ca7cd8dfc4fefba380be98d05a2fc1
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-10-10 14:13:36 +0300

    refactor: improve styles for Pinwheel hero

--- src/widget/CasesHero/Pinwheel/Pinwheel.tsx
+++ src/widget/CasesHero/Pinwheel/Pinwheel.tsx
@@ -2,18 +2,22 @@ import { FC } from "react";
 
 import styles from "./Pinwheel.module.css";
 
-import { HeroTitle, HeroTitleProps } from "../ui/HeroTitle/HeroTitle";
+import { TitleProps } from "@/shared/ui/Title/Title";
+
 import { Keyword } from "../ui/Keyword/Keyword";
 
 export type PinwheelProps = {
   keywords: string[];
-} & HeroTitleProps;
+} & TitleProps;
 
 export const Pinwheel: FC<PinwheelProps> = ({ title, subtitle, keywords }) => {
   return (
     <section className={styles.hero}>
       <div className={styles.content}>
-        <HeroTitle title={title} subtitle={subtitle} />
+        <div className={styles.text}>
+          <h1 className={styles.title}>{title}</h1>
+          {subtitle && <p className={styles.subtitle}>{subtitle}</p>}
+        </div>
         <div className={styles.keywords}>
           {keywords.map((k, idx) => (
             <Keyword key={idx} keyword={k} />
