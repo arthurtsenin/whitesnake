a91263491a8085d01041c09b966622992309ee4d
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-10-10 13:42:05 +0300

    refactor: improve styles for Oddbox hero

--- src/widget/CasesHero/Oddbox/Oddbox.tsx
+++ src/widget/CasesHero/Oddbox/Oddbox.tsx
@@ -3,20 +3,24 @@ import { FC } from "react";
 
 import styles from "./Oddbox.module.css";
 
-import { HeroTitle, HeroTitleProps } from "../ui/HeroTitle/HeroTitle";
+import { TitleProps } from "@/shared/ui/Title/Title";
+
 import { Keyword } from "../ui/Keyword/Keyword";
 
 import heroBg from "&/cases/food-delivery-service.png";
 
 export type OddboxProps = {
   keywords: string[];
-} & HeroTitleProps;
+} & TitleProps;
 
 export const Oddbox: FC<OddboxProps> = ({ title, subtitle, keywords }) => {
   return (
     <section className={styles.hero}>
       <div className={styles.content}>
-        <HeroTitle title={title} subtitle={subtitle} />
+        <div className={styles.text}>
+          <h1 className={styles.title}>{title}</h1>
+          {subtitle && <p className={styles.subtitle}>{subtitle}</p>}
+        </div>
         <div className={styles.imgWrapper}>
           <Image src={heroBg} alt="" priority />
         </div>
