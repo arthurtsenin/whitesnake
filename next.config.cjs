7289335f03f92bfe75aaebd0692342486b506a63
Sega <Segabelka@yandex.ru>
2023-10-15 20:37:58 +0300

    fix: add preload for HSTS

--- next.config.cjs
+++ next.config.cjs
@@ -7,7 +7,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Strict-Transport-Security',
-            value: 'max-age=31536000; includeSubDomains',
+            value: 'max-age=31536000; includeSubDomains; preload',
           },
         ],
       },
