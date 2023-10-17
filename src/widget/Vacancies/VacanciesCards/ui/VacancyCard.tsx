c0d68a84e7167ef2ba9e43c85df114dd9c6b6450
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-10-17 17:42:30 +0300

    feat: design vacancy card

--- src/widget/Vacancies/VacanciesCards/ui/VacancyCard.tsx
+++ src/widget/Vacancies/VacanciesCards/ui/VacancyCard.tsx
@@ -1,4 +1,11 @@
+import cn from "classnames";
+import Image from "next/image";
+import Link from "next/link";
 import { FC } from "react";
+
+import styles from "./VacancyCard.module.css";
+
+import arrow from "&/arrow-right-white.svg";
 
 export type VacancyCardType = {
   title: string;
@@ -8,6 +15,16 @@ export type VacancyCardType = {
 
 type VacancyCardProps = object & VacancyCardType;
 
-export const VacancyCard: FC<VacancyCardProps> = () => {
-  return <div>xxxxxxxxxx</div>;
+export const VacancyCard: FC<VacancyCardProps> = ({ title, location, url }) => {
+  return (
+    <div className={styles.container}>
+      <Link href={url} className={styles.text}>
+        <p className={styles.title}>{title}</p>
+        <p className={styles.description}>{location}</p>
+        <div className={cn(styles.arrow, styles.hover)}>
+          <Image src={arrow} alt="arrow" />
+        </div>
+      </Link>
+    </div>
+  );
 };
