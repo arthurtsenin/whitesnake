03b6a5f97bb3baa5931ce1653af972971222330b
unknown <milena.mahnach@innowise-group.com>
2023-10-18 12:49:56 +0300

    style: fix styles for footer

--- src/widget/BenefitCards/BenefitCards.tsx
+++ src/widget/BenefitCards/BenefitCards.tsx
@@ -4,28 +4,25 @@ import styles from "./BenefitCards.module.css";
 
 import { BenefitCardData } from "./types";
 import { BenefitCard } from "./ui/BenefitCard";
-import { Container } from "../../shared";
 
-type BenefitCardsProps = {
+export type BenefitCardsProps = {
   variant: "withIcon" | "withoutIcon";
   data: BenefitCardData[];
 };
 
 export const BenefitCards: FC<BenefitCardsProps> = ({ variant, data }) => {
   return (
-    <Container>
-      <div className={styles.benefitCardsWraper}>
-        {data.map((item) => (
-          <BenefitCard
-            key={item.cardTitle}
-            cardTitle={item.cardTitle}
-            cardDesc={item.cardDesc}
-            buttonText={item.buttonText}
-            variant={variant}
-            icon={item.icon}
-          />
-        ))}
-      </div>
-    </Container>
+    <div className={styles.benefitCardsWraper}>
+      {data.map((item) => (
+        <BenefitCard
+          key={item.cardTitle}
+          cardTitle={item.cardTitle}
+          cardDesc={item.cardDesc}
+          buttonText={item.buttonText}
+          variant={variant}
+          icon={item.icon}
+        />
+      ))}
+    </div>
   );
 };
