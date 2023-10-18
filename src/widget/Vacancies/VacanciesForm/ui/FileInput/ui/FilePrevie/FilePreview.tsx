b670e520e9d537f1657661ece103494502e5af00
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-10-18 20:31:50 +0300

    feat: implement ui, action for vacancies form

---
+++ src/widget/Vacancies/VacanciesForm/ui/FileInput/ui/FilePrevie/FilePreview.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import { FC } from "react";
+
+import styles from "./FilePrevie.module.css";
+
+import complete from "&/images/vacancies/complete.png";
+
+type FilePreviewProps = {
+  name: string;
+};
+export const FilePreview: FC<FilePreviewProps> = ({ name }) => {
+  return (
+    <div className={styles.helper}>
+      <Image className={styles.icon} src={complete} alt="complete" />
+      <span className={styles.text}>{name}</span>
+    </div>
+  );
+};
