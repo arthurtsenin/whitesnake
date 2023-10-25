df495d28eff1732713e9f565f10b33788dfead3e
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-10-25 17:40:31 +0300

    feat: create hero for career

---
+++ src/widget/Career/CareerHero/CareerHero.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FC } from "react";
+
+import styles from "./CareerHero.module.css";
+
+import { Button } from "@/shared";
+import { NAVIGATION_LINKS } from "@/widget/Navigation/routes";
+
+import raindrop from "&/images/vacancies/hero/raindrop-lower.png";
+import raindropPurple from "&/images/vacancies/hero/raindrop-upper.png";
+
+type CareerHeroProps = {
+  path: string;
+};
+
+export const CareerHero: FC<CareerHeroProps> = ({ path }) => {
+  return (
+    <div className={styles.hero}>
+      <div className={styles.glowRed} />
+      <div className={styles.glowBlue} />
+
+      <div className={styles.raindrop1}>
+        <Image src={raindrop} alt="" />
+      </div>
+
+      <div className={styles.raindrop2}>
+        <Image src={raindrop} alt="" />
+      </div>
+
+      <div className={styles.raindrop3}>
+        <Image src={raindropPurple} alt="" />
+      </div>
+
+      <div className={styles.raindrop4}>
+        <Image src={raindropPurple} alt="" />
+      </div>
+
+      <div className={styles.content}>
+        <h1 className={styles.title}>Карьера</h1>
+        <p className={styles.subtitle}>
+          Кто мы и как живет наша компания в целом. Вдохновлящие слова
+        </p>
+
+        <div className={styles.buttons}>
+          <Link href={NAVIGATION_LINKS.vacancies.href}>
+            <Button variant="primary">Вакансии</Button>
+          </Link>
+          <Link target="_blank" href={path}>
+            <Button variant="transparent">Перейти на rabota.by</Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
