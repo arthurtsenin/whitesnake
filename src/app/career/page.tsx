cebe19a6e6d724ca837a124580bf8125797bd796
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-10-25 16:11:35 +0300

    refactor: clear career page

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -1,17 +1,9 @@
 import PageScript from "@/shared/analitics/PageScript";
-import { VacanciesHero } from "@/widget/Vacancies/VacanciesHero/VacanciesHero";
-import { WORKING_CONDITIONS_DATA } from "@/widget/WorkingConditions/data";
-import { WorkingConditions } from "@/widget/WorkingConditions/WorkingConditions";
 
 export default function CareerPage() {
   return (
     <>
       <PageScript />
-      <VacanciesHero path="#" />
-      <WorkingConditions
-        title="Что такое работа в Whitesnake"
-        conditions={WORKING_CONDITIONS_DATA}
-      />
     </>
   );
 }
