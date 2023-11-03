ac31047c345a657f3a9060fb47fc6a6e14a202bb
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-11-03 17:54:42 +0300

    feat: add bg to cards, adjust styles for text and bg

--- src/widget/Career/Benefits/Benefits.tsx
+++ src/widget/Career/Benefits/Benefits.tsx
@@ -2,10 +2,11 @@ import { FC } from "react";
 
 import styles from "./Benefits.module.css";
 
+import { Container } from "@/shared";
+
 import { BenefitType } from "./data";
 import { BenefitCard } from "./ui/BenefitCard/BenefitCard";
-import { Title } from "../ui/Title/Title";
-import { TitleProps } from "../ui/Title/Title";
+import { Title, TitleProps } from "../ui/Title/Title";
 
 type BenefitsProps = {
   benefits: Array<BenefitType>;
@@ -13,18 +14,21 @@ type BenefitsProps = {
 
 export const Benefits: FC<BenefitsProps> = ({ title, benefits }) => {
   return (
-    <section className={styles.section}>
-      <Title title={title} />
+    <Container>
+      <section className={styles.section} style={{ marginTop: "350px" }}>
+        <Title title={title} />
 
-      <div className={styles.container}>
-        {benefits.map((card) => (
-          <BenefitCard
-            key={card.id}
-            title={card.title}
-            description={card.description}
-          />
-        ))}
-      </div>
-    </section>
+        <div className={styles.container}>
+          {benefits.map((card) => (
+            <BenefitCard
+              key={card.id}
+              title={card.title}
+              description={card.description}
+              imageSrc={card.imageSrc}
+            />
+          ))}
+        </div>
+      </section>
+    </Container>
   );
 };
--- src/widget/Career/Benefits/ui/BenefitCard/BenefitCard.tsx
+++ src/widget/Career/Benefits/ui/BenefitCard/BenefitCard.tsx
@@ -1,3 +1,4 @@
+import Image, { StaticImageData } from "next/image";
 import { FC } from "react";
 
 import styles from "./BenefitCard.module.css";
@@ -5,13 +6,22 @@ import styles from "./BenefitCard.module.css";
 type BenefitCardProps = {
   title: string;
   description: string;
+  imageSrc: StaticImageData;
 };
 
-export const BenefitCard: FC<BenefitCardProps> = ({ title, description }) => {
+export const BenefitCard: FC<BenefitCardProps> = ({
+  title,
+  description,
+  imageSrc,
+}) => {
   return (
     <div className={styles.card}>
-      <p className={styles.title}>{title}</p>
-      <p className={styles.description}>{description}</p>
+      <Image src={imageSrc} alt="" />
+
+      <div className={styles.text}>
+        <p className={styles.title}>{title}</p>
+        <p className={styles.description}>{description}</p>
+      </div>
     </div>
   );
 };
