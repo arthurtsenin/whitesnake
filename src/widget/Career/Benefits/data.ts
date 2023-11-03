2be68dde0b0def89e4247d450862accc013e847e
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-11-03 17:53:29 +0300

    feat: add image to data for benefit cards

--- src/widget/Career/Benefits/data.ts
+++ src/widget/Career/Benefits/data.ts
@@ -1,7 +1,17 @@
+import { StaticImageData } from "next/image";
+
+import benefitCardBg1 from "&/images/career/benefit-card-bg-1.webp";
+import benefitCardBg2 from "&/images/career/benefit-card-bg-2.webp";
+import benefitCardBg3 from "&/images/career/benefit-card-bg-3.webp";
+import benefitCardBg4 from "&/images/career/benefit-card-bg-4.webp";
+import benefitCardBg5 from "&/images/career/benefit-card-bg-5.webp";
+import benefitCardBg6 from "&/images/career/benefit-card-bg-6.webp";
+
 export type BenefitType = {
   id: number;
   title: string;
   description: string;
+  imageSrc: StaticImageData;
 };
 
 export const BENEFITS_DATA: Array<BenefitType> = [
@@ -10,32 +20,39 @@ export const BENEFITS_DATA: Array<BenefitType> = [
     title: "Забота о здоровье",
     description:
       "Наша компания помогает повышать доступность медицинских услуг.",
+    imageSrc: benefitCardBg1,
   },
   {
     id: 2,
     title: "Sick Days",
     description:
       "В первый год работы доступно 3 дня в год, а начиная со второго 5 дней в год.",
+    imageSrc: benefitCardBg2,
   },
   {
     id: 3,
     title: "Спорт и хобби",
     description: "Частичная компенсация расходов на ваше любимое дело",
+    imageSrc: benefitCardBg3,
   },
   {
     id: 4,
     title: "Техника",
-    description: "Педоставляем помощь в приобретении необходимого оборудования",
+    description:
+      "Предоставляем помощь в приобретении необходимого оборудования",
+    imageSrc: benefitCardBg4,
   },
   {
     id: 5,
     title: "Сытый сотрудник",
     description: "Компания частично компенсирует расходы потраченные на обеды",
+    imageSrc: benefitCardBg5,
   },
   {
     id: 6,
     title: "Транспорт",
     description:
       "Компания поможет вам сэкономить деньги, потраченные на дорогу в офис.",
+    imageSrc: benefitCardBg6,
   },
 ];
