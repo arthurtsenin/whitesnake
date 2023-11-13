dde846d952a762026e485eb7c5d0f30dcf06c262
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-11-14 02:42:55 +0300

    refactor: rewrite layout and css for case cards

--- src/widget/GridContainer/ui/CaseCard/CaseCard.tsx
+++ src/widget/GridContainer/ui/CaseCard/CaseCard.tsx
@@ -6,8 +6,6 @@ import styles from "./CaseCard.module.css";
 
 import { NAVIGATION_LINKS } from "@/widget/Navigation/routes";
 
-import arrow from "&/arrow-right.svg";
-
 type CaseCardProps = {
   caption: string;
   description: string;
@@ -23,22 +21,17 @@ export const CaseCard: FC<CaseCardProps> = ({
 }) => {
   return (
     <figure className={styles.container}>
-      <div className={styles.wrapper}>
-        <Image src={image} alt={caption} placeholder="blur" />
-        <Link
-          href={`${NAVIGATION_LINKS.portfolio.href}/${path}`}
-          className={styles.hover}
-        >
-          <div className={styles.arrow}>
-            <Image
-              src={arrow}
-              alt="Перехода на страницу с детальной информацией."
-            />
-          </div>
-          <p className={styles.description}>{description}</p>
-        </Link>
-      </div>
+      <Image src={image} alt={caption} placeholder="blur" />
+
       <figcaption className={styles.caption}>{caption}</figcaption>
+
+      <Link
+        href={`${NAVIGATION_LINKS.portfolio.href}/${path}`}
+        className={styles.hover}
+      >
+        <p className={styles.description}>{description}</p>
+        <span className={styles.further}>Подробнее</span>
+      </Link>
     </figure>
   );
 };
