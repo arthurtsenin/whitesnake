135ea1928ef0f2b368792a5454ea911726d8cee5
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-11-16 20:35:46 +0300

    feat: layout cards statically into tapper

--- src/widget/HiringProcess/HiringProcess.tsx
+++ src/widget/HiringProcess/HiringProcess.tsx
@@ -7,6 +7,7 @@ import { Title, TitleProps } from "@/shared/ui/Title/Title";
 
 import { HiringProcessCardType } from "./data";
 import { HiringProcessCard } from "./ui/HiringProcessCard/HiringProcessCard";
+import { Indicators } from "./ui/Indicators/Indicators";
 
 type HiringProcessProps = {
   cards: Array<HiringProcessCardType>;
@@ -18,15 +19,21 @@ export const HiringProcess: FC<HiringProcessProps> = ({ title, cards }) => {
       <section className={styles.section} style={{ marginTop: "350px" }}>
         <Title title={title} />
 
-        <div className={styles.container}>
-          {cards.map((card) => (
-            <HiringProcessCard
-              key={card.id}
-              title={card.title}
-              instructions={card.instructions}
-              image={card.image}
-            />
-          ))}
+        <div className={styles.external}>
+          <div className={styles.container}>
+            <div className={styles.inner}>
+              {cards.map((card) => (
+                <HiringProcessCard
+                  key={card.id}
+                  title={card.title}
+                  instructions={card.instructions}
+                  image={card.image}
+                />
+              ))}
+            </div>
+
+            <Indicators ids={[1, 2, 3]} activeItemId={1} />
+          </div>
         </div>
       </section>
     </Container>
