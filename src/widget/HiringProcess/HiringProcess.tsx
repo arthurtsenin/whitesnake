2b02fe0d2b72d46c7092ec32f982dcdc1c312f1d
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-11-17 15:13:32 +0300

    feat: implement switch slides via scroll-snap

--- src/widget/HiringProcess/HiringProcess.tsx
+++ src/widget/HiringProcess/HiringProcess.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FC, useEffect, useRef, useState } from "react";
+import { FC, useRef, useState } from "react";
 
 import styles from "./HiringProcess.module.css";
 
@@ -17,25 +17,12 @@ type HiringProcessProps = {
 
 export const HiringProcess: FC<HiringProcessProps> = ({ title, cards }) => {
   const [activeItemId, setActiveItemId] = useState<number>(0);
-  const [direction, _setDirection] = useState<number>(1);
-  const isMobile = window.innerWidth < 744;
+  // const [direction, _setDirection] = useState<number>(1);
   const innerRef = useRef<HTMLDivElement>(null);
 
   const ids = cards.map((card) => card.id as number);
 
-  const handleClick = () => {
-    setActiveItemId((prev) => prev + 1);
-  };
-
   // TODO: handle resize
-
-  useEffect(() => {
-    const newIndex = direction > 0 ? activeItemId + 1 : activeItemId - 1;
-
-    if (isMobile && innerRef && innerRef.current) {
-      innerRef.current.style.transform = `translateX(calc(-${newIndex} * ((80vw - 48px)) / 1.5))`;
-    }
-  }, [direction, activeItemId, isMobile]);
 
   return (
     <Container>
@@ -44,15 +31,17 @@ export const HiringProcess: FC<HiringProcessProps> = ({ title, cards }) => {
 
         <div className={styles.external}>
           <div className={styles.container}>
-            <div className={styles.inner} ref={innerRef}>
+            <div className={styles.inner} ref={innerRef} id="inner">
               {cards.map((card) => (
                 <HiringProcessCard
                   key={card.id}
+                  id={card.id}
                   title={card.title}
                   instructions={card.instructions}
                   image={card.image}
                   isActive={card.id === activeItemId}
-                  onClick={card.id === activeItemId ? undefined : handleClick}
+                  setActiveItemId={setActiveItemId}
+                  // containerRef={innerRef}
                 />
               ))}
             </div>
--- src/widget/HiringProcess/ui/HiringProcessCard/HiringProcessCard.tsx
+++ src/widget/HiringProcess/ui/HiringProcessCard/HiringProcessCard.tsx
@@ -1,6 +1,6 @@
 import cn from "classnames";
 import Image from "next/image";
-import { FC, MouseEventHandler } from "react";
+import { FC, useEffect, useRef } from "react";
 
 import styles from "./HiringProcessCard.module.css";
 
@@ -8,24 +8,52 @@ import { HiringProcessCardType } from "../../data";
 
 type HiringProcessCardProps = {
   isActive: boolean;
-  onClick: MouseEventHandler<HTMLDivElement> | undefined;
+  setActiveItemId: (num: number) => void;
 } & HiringProcessCardType;
 
 export const HiringProcessCard: FC<HiringProcessCardProps> = ({
+  id,
   title,
   instructions,
   image,
   isActive,
-  // onPrevClick,
-  // onNextClick,
-  onClick,
+  setActiveItemId,
+  // containerRef,
 }) => {
+  const itemRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const callback = (entries: Array<IntersectionObserverEntry>) => {
+      entries.forEach((entry) => {
+        if (entry.intersectionRatio > 0.75) {
+          const newId = Number(entry.target.dataset.id);
+          setActiveItemId(newId);
+        }
+      });
+    };
+
+    const options = {
+      root: document.querySelector("#inner"),
+      rootMargin: "0px",
+      threshold: 1,
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+
+    if (itemRef && itemRef.current) {
+      observer.observe(itemRef.current);
+    }
+
+    // return () => unobserve
+  }, [setActiveItemId]);
+
   return (
     <div
       className={cn(styles.container, {
         [styles.active]: isActive,
       })}
-      onClick={onClick}
+      data-id={id}
+      ref={itemRef}
     >
       <div className={styles.wrapper}>
         <Image alt="" placeholder="blur" src={image} />
