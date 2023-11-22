7b6181223acc3db478488032419a51f6f90f8d2f
milena666-0-0 <milena290105@gmail.com>
2023-11-22 14:25:21 +0300

    refactor: remove unnecessary components from about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,18 +1,10 @@
-import { Container } from "@/shared";
 import PageScript from "@/shared/analitics/PageScript";
-import { CompanyValues } from "@/widget/CompanyValues/CompanyValues";
-import { HomeHero } from "@/widget/HomeHero/HomeHero";
-import { InternshipWorkBenefits } from "@/widget/Internship/InternshipWorkBenefits/InternshipWorkBenefits";
 
 export default function AboutPage() {
   return (
     <>
       <PageScript />
-      <Container>
-        <HomeHero />
-        <CompanyValues />
-        <InternshipWorkBenefits />
-      </Container>
+      About page
     </>
   );
 }
