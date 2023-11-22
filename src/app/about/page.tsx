d2e1a20c7c846e02e8d487d14d3e120584e45cb5
milena666-0-0 <milena290105@gmail.com>
2023-11-22 14:18:16 +0300

    feat: create company values component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,18 @@
+import { Container } from "@/shared";
 import PageScript from "@/shared/analitics/PageScript";
+import { CompanyValues } from "@/widget/CompanyValues/CompanyValues";
+import { HomeHero } from "@/widget/HomeHero/HomeHero";
+import { InternshipWorkBenefits } from "@/widget/Internship/InternshipWorkBenefits/InternshipWorkBenefits";
 
 export default function AboutPage() {
   return (
     <>
       <PageScript />
-      About page
+      <Container>
+        <HomeHero />
+        <CompanyValues />
+        <InternshipWorkBenefits />
+      </Container>
     </>
   );
 }
