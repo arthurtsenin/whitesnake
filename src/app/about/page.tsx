4ef677c506c8acb38422a63dec0a5f8d233d45b4
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-11-23 12:49:27 +0300

    feat: add two components to about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,14 @@
 import PageScript from "@/shared/analitics/PageScript";
+import { CompanyNumbers } from "@/widget/AboutUs/CompanyNumbers/CompanyNumbers";
+import { COMPANY_NUMBERS_DATA } from "@/widget/AboutUs/CompanyNumbers/data";
+import { InternshipOffice } from "@/widget/Internship/InternshipOffice/InternshipOffice";
 
 export default function AboutPage() {
   return (
     <>
       <PageScript />
-      About page
+      <CompanyNumbers title="Немного цифр" cards={COMPANY_NUMBERS_DATA} />
+      <InternshipOffice />
     </>
   );
 }
