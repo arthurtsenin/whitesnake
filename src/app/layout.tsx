1b119769d48391a92651a0fe4d144fd0c42f8128
arthurtsenin <arturcenin@gmail.com>
2023-11-24 16:24:48 +0300

    feat: add about us page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
   const [mystate, setMystate] = useState(false);
   const [TbFP, setTbFP] = useState(false);
   const router = useRouter();
+
   useLayoutEffect(() => {
     if (
       typeof window !== "undefined" &&
--- src/widget/YandexMap/ui/MapPreview/MapPreview.tsx
+++ src/widget/YandexMap/ui/MapPreview/MapPreview.tsx
@@ -4,26 +4,14 @@ import { FC } from "react";
 
 import styles from "./MapPreview.module.css";
 
-import { Container } from "@/shared";
 import { YANDEX_MAP_URL } from "@/shared/constants/map";
 
 import map from "&/images/internship/map.png";
 
 export const MapPreview: FC = () => {
   return (
-    <Container>
-      <Link target="_blank" href={YANDEX_MAP_URL} className={styles.wrapper}>
-        <Image
-          alt=""
-          src={map}
-          placeholder="blur"
-          fill
-          sizes="100vw"
-          style={{
-            objectFit: "cover",
-          }}
-        />
-      </Link>
-    </Container>
+    <Link target="_blank" href={YANDEX_MAP_URL} className={styles.wrapper}>
+      <Image alt="" src={map} placeholder="blur" fill />
+    </Link>
   );
 };
