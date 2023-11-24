b7eb6892a881845f2a94e82d99a7babb2b16120f
arthurtsenin <arturcenin@gmail.com>
2023-11-24 14:35:59 +0300

    fix: remove useless import

--- src/app/internship/page.tsx
+++ src/app/internship/page.tsx
@@ -2,7 +2,6 @@ import PageScript from "@/shared/analitics/PageScript";
 import { Advantages } from "@/widget/Advantages/Advantages";
 import { VacancyForm } from "@/widget/Form/VacancyForm/VacancyForm";
 import { InternshipAccardion } from "@/widget/Internship/InternshipAccardion/InternshipAccardion";
-// import { InternshipForm } from "@/widget/Internship/InternshipForm/InternshipForm";
 import { InternshipHero } from "@/widget/Internship/InternshipHero/InternshipHero";
 import { InternshipOffice } from "@/widget/Internship/InternshipOffice/InternshipOffice";
 import { InternshipWorkBenefits } from "@/widget/Internship/InternshipWorkBenefits/InternshipWorkBenefits";
