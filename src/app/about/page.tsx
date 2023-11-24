fbd7a72e76624e54d9ae641c2fd8d077f0368eff
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-11-24 12:48:42 +0300

    feat: import CompnyValues component to page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,6 +2,7 @@ import PageScript from "@/shared/analitics/PageScript";
 import { AboutHero } from "@/widget/AboutUs/AboutHero/AboutHero";
 import { CompanyNumbers } from "@/widget/AboutUs/CompanyNumbers/CompanyNumbers";
 import { COMPANY_NUMBERS_DATA } from "@/widget/AboutUs/CompanyNumbers/data";
+import { CompanyValues } from "@/widget/CompanyValues/CompanyValues";
 import { InternshipOffice } from "@/widget/Internship/InternshipOffice/InternshipOffice";
 
 export default function AboutPage() {
@@ -9,6 +10,7 @@ export default function AboutPage() {
     <>
       <PageScript />
       <AboutHero />
+      <CompanyValues />
       <CompanyNumbers title="Немного цифр" cards={COMPANY_NUMBERS_DATA} />
       <InternshipOffice />
     </>
